cd71674e1f8eab01fe0085adc29fe69d82506e4d
Erfan-267 <erfancraft26@gmail.com>
2025-11-03 20:59:58 +0330

    wad

--- script/account.js
+++ script/account.js
@@ -1,5 +1,5 @@
 
-    $('.btn-toast').on('click',function(e){
+$('.btn-toast').on('click',function(e){
   e.preventDefault();
   $.toast({
     text: 'برای خرید باید احراز هویت خود را کامل کنید',
@@ -19,3 +19,25 @@
     afterHidden:function(){}
   });
 })
+
+let proPage=document.querySelectorAll('.profile-data-script');
+let proBtn=document.querySelectorAll('.data-link');
+let proBtnText=document.querySelectorAll('.proBtnText');
+for(let i=0;i<=proBtn.length-1;i++){
+  proBtn[i].addEventListener('click',()=>{
+    proBtnText[i].classList.add('text-[#FFFF00]');
+    for(let j=0;j<=proBtnText.length-1;j++){
+      if(i != j){
+        proBtnText[j].classList.remove('text-[#FFFF00]');
+      }
+    }
+    proPage[i].classList.remove('hidden');
+    for(let q=0;q<=proPage.length-1;q++){
+      if(q !== i){
+        proPage[q].classList.add('hidden');
+      }
+    }
+  })
+}
+
+
